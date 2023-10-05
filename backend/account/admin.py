582b1672c367b63bb70ea1277d941df5db32e8cf
Galdanwing <antoinevanderhorst@proton.me>
2023-10-05 13:31:48 +0200

    refactor: "Improve Django admin interface for Profile and Inventory models

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-
 from account.models import Inventory, Profile
 
-# Register your models here.
 
-admin.site.register(Profile)
-admin.site.register(Inventory)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user',)  # display user in list
+
+    # make models name more readable in admin interface
+    def __str__(self):
+        return self.user.username
+
+
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user',)  # display name and user in list
+
+    # make models name more readable in admin interface
+    def __str__(self):
+        return self.name
+
+
+# Register your models here.
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Inventory, InventoryAdmin)

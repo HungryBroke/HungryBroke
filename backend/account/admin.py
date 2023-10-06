ea6e831188ae5c9bf4627420a94aede4e925ede0
Galdanwing <antoinevanderhorst@proton.me>
2023-10-06 14:22:53 +0200

    feat: "Register models using decorators in admin.py
    The previous code was registering Profile and Inventory models directly using the admin.site.register method. This has been updated to use decorators, @admin.register(Profile) and @admin.register(Inventory), which is a more modern approach. It keeps the code cleaner and more readable."

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,17 +1,23 @@
 from django.contrib import admin
+
 from account.models import Inventory, Profile
 
 
+@admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user',)  # display user in list
+    list_display = ("user",)  # display user in list
 
     # make models name more readable in admin interface
     def __str__(self):
         return self.user.username
 
 
+@admin.register(Inventory)
 class InventoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'user',)  # display name and user in list
+    list_display = (
+        "name",
+        "user",
+    )  # display name and user in list
 
     # make models name more readable in admin interface
     def __str__(self):
@@ -19,5 +25,3 @@ class InventoryAdmin(admin.ModelAdmin):
 
 
 # Register your models here.
-admin.site.register(Profile, ProfileAdmin)
-admin.site.register(Inventory, InventoryAdmin)
